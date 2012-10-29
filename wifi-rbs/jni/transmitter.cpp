d75f6f87aae8ec15eea4e67cce9d51bb4690c69b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-10-29 18:56:21 -0400

    Change port numbers, send only at 10 Hz.

--- wifi-rbs/jni/transmitter.cpp
+++ wifi-rbs/jni/transmitter.cpp
@@ -4,7 +4,7 @@
 int main() {
   /* Create WiFi transmitter socket */
   Socket wifi_tx_sock;
-  Socket::Address wifi_addr("192.168.5.1",9001);
+  Socket::Address wifi_addr("192.168.5.1",18001);
   wifi_tx_sock.bind(wifi_addr);
   wifi_tx_sock.bind_to_device("wlan0");
 
@@ -12,19 +12,19 @@ int main() {
   wifi_tx_sock.enable_broadcast();
 
  /* Keep transmitting broadcast packets */
- Socket::Address bcast("192.168.5.255",9000);
+ Socket::Address bcast("192.168.5.255",18000);
  uint64_t packets_sent=0;
  uint64_t last_time=0;
  while(1) {
   uint64_t the_time = Socket::timestamp();
-  if ( the_time > last_time + 10e6 ) { /* 10 ms */
+  if ( the_time > last_time + 100e6 ) { /* 100 ms */
    packets_sent++;
    char *seq_encoded = (char *)&packets_sent;
    wifi_tx_sock.send( Socket::Packet(bcast, std::string( seq_encoded, 51 ) ) );
    last_time=the_time;
   }
   else {
-   usleep((last_time + 10e6 - the_time)/(1.0e3)); /* ns to us conversion */
+   usleep((last_time + 100e6 - the_time)/(1.0e3)); /* ns to us conversion */
   }
  }
 }
